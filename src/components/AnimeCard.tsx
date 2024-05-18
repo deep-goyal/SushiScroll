47e88bd8913ea96d5aa74e141c3ddf2e8c2ab1cd
Deep Goyal <deep.goyal@icloud.com>
2024-05-18 17:19:55 +0530

    add avg trend score badge

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Anime } from "../hooks/useFetchAnime";
+import AverageScore from "./AverageScore";
 
 interface Props {
   anime: Anime;
@@ -11,7 +12,10 @@ const AnimeCard = ({ anime }: Props) => {
     <Card borderRadius={10}>
       <Image src={anime.coverImage} overflow="hidden" />
       <CardBody>
-        <Heading fontSize="xl">{anime.title.toLocaleUpperCase()}</Heading>
+        <HStack justifyContent={"space-between"}>
+          <Heading fontSize="xl">{anime.title.toLocaleUpperCase()}</Heading>
+          <AverageScore avgscore={anime.rating} />
+        </HStack>
       </CardBody>
     </Card>
   );
---
+++ src/components/AverageScore.tsx
@@ -0,0 +1,17 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  avgscore: number;
+}
+
+const AverageScore = ({ avgscore }: Props) => {
+  let color = avgscore > 80 ? "green" : avgscore > 60 ? "yellow" : "red";
+  return (
+    <Badge fontSize={"14px"} paddingX={1} colorScheme={color}>
+      {avgscore}
+    </Badge>
+  );
+};
+
+export default AverageScore;
